5b9e3cd1aa1705c55ce447a3b7cbde92da5079f3
slimit75 <slimit75@pm.me>
2023-07-05 10:04:15 -0400

    feat: improved error handling

--- index.js
+++ index.js
@@ -86,43 +86,86 @@ async function fetchData() {
 // Chatbox Command Handler
 sc.on("command", async (cmd) => {
     if (aliases.includes(cmd.command)) {
-        if ((cmd.args[0] == null) || (cmd.args[0] == "help")) {
-            // Help message
-            await sc.tell(cmd.user.name, `FindShop helps locate ShopSync-compatible shops buying or selling an item.\n\`\\fs list\` - List detected shops\n\`\\fs stats\` - Statistics (currently only shop count)\n\`\\fs buy [item]\` - Finds shops selling *[item]*\n\`\\fs sell [item]\` - Finds shops buying *[item]*\n\`\\fs shop [name]\` - Finds shops named *[name]* and their info`)
-        }
-        else if (cmd.args[0] == "stats") {
-            // Link to stats dashboard
-            await sc.tell(cmd.user.name, `Detailed shop statistics can be viewed [here](https://charts.mongodb.com/charts-findshop-lwmvk/public/dashboards/649f2873-58ae-45ef-8079-03201394a531).`);
-        }
-        else if ((cmd.args[0] == "list") || (cmd.args[0] == "l")) {
-            // List shops
-            const shops = await fetchData();
-            let printResults = "";
-
-            for (const shop of shops) {
-                printResults += `\n**${ shop.info.name }** at \`${ genCoords(shop.info.location) }\``;
+        console.debug(`${ cmd.user.name }: ${cmd.args.join(" ")}`);
+        try {
+            if ((cmd.args[0] == null) || (cmd.args[0] == "help")) {
+                // Help message
+                await sc.tell(cmd.user.name, `FindShop helps locate ShopSync-compatible shops buying or selling an item.\n\`\\fs list\` - List detected shops\n\`\\fs stats\` - Statistics (currently only shop count)\n\`\\fs buy [item]\` - Finds shops selling *[item]*\n\`\\fs sell [item]\` - Finds shops buying *[item]*\n\`\\fs shop [name]\` - Finds shops named *[name]* and their info`)
             }
-
-            await sc.tell(cmd.user.name, `FindShop found the following shops:\n${ printResults }`);
-        }
-        else if ((cmd.args[0] == "buy") || (cmd.args[0] == "b") || (cmd.args[1] == null)) {
-            // Find shops selling search_item
-            let search_item = cmd.args[1];
-            if (cmd.args[1] == null) {
-                search_item = cmd.args[0];
+            else if (cmd.args[0] == "stats") {
+                // Link to stats dashboard
+                await sc.tell(cmd.user.name, `Detailed shop statistics can be viewed [here](https://charts.mongodb.com/charts-findshop-lwmvk/public/dashboards/649f2873-58ae-45ef-8079-03201394a531).`);
             }
-            console.log(`Searching for shops selling ${ search_item }`);
+            else if ((cmd.args[0] == "list") || (cmd.args[0] == "l")) {
+                // List shops
+                const shops = await fetchData();
+                let printResults = "";
 
-            const shops = await fetchData();
-            const results = [];
-            for (const shop of shops) {
-                // Check item length because if this is zero, it will crash!!! Thanks books.kst
-                if (shop.items.length > 0) {
+                for (const shop of shops) {
+                    printResults += `\n**${ shop.info.name }** at \`${ genCoords(shop.info.location) }\``;
+                }
+
+                await sc.tell(cmd.user.name, `FindShop found the following shops:\n${ printResults }`);
+            }
+            else if ((cmd.args[0] == "buy") || (cmd.args[0] == "b") || (cmd.args[1] == null)) {
+                // Find shops selling search_item
+                let search_item = cmd.args[1];
+                if (cmd.args[1] == null) {
+                    search_item = cmd.args[0];
+                }
+
+                const shops = await fetchData();
+                const results = [];
+                for (const shop of shops) {
+                    // Check item length because if this is zero, it will crash!!! Thanks books.kst
+                    if (shop.items.length > 0) {
+                        for (const item of shop.items) {
+                            if (item.item.name == null) {
+                                console.warn(`A shop (${shop.info.name}) is missing an item name!!`);
+                            }
+                            else if (((item.item.name.toLowerCase().includes(search_item.toLowerCase())) || (item.item.displayName.toLowerCase().includes(search_item.toLowerCase()))) && (!item.shopBuysItem) && ((item.stock != 0) || (item.madeOnDemand))) {
+                                results.push({
+                                    shop: shop.info,
+                                    item: item
+                                })
+                            }
+                        }
+                    }
+                    else {
+                        console.warn(`A shop (${shop.info.name}) is broadcasting an empty items array!!`)
+                    }
+                }
+
+                if (results.length == 0) {
+                    await sc.tell(cmd.user.name, `**Error!** FindShop was unable to find any shops with \`${ search_item }\` in stock. [Why are shops and items missing?](${help_link})`);
+                }
+                else {
+                    let printResults = "";
+
+                    if (results.length > 5) {
+                        await sc.tell(cmd.user.name, "**Note:** Too many results found. Shorting the list to the first 5 results.");
+                        results.length = 5;
+                    }
+
+                    for (const result of results) {
+                        printResults += `\n\`${ result.item.item.name }\` at **${ result.shop.name }** (\`${ genCoords(result.shop.location) }\`) for ${ fmt_price(result.item) } (\`${ result.item.stock }\` in stock)`;
+                    }
+
+                    await sc.tell(cmd.user.name, `Here's what we found for \`${ search_item }\`: ${ printResults }`);
+                }
+            }
+            else if ((cmd.args[0] == "sell") || (cmd.args[0] == "sl")) {
+                // Find shops buying search_item
+                const search_item = cmd.args[1];
+
+                const shops = await fetchData();
+                const results = [];
+                for (const shop of shops) {
                     for (const item of shop.items) {
                         if (item.item.name == null) {
-                            console.debug(`A shop (${shop.info.name}) is missing an item name!!`);
+                            console.warn(`A shop (${shop.info.name}) is missing an item name!!`);
                         }
-                        else if (((item.item.name.toLowerCase().includes(search_item.toLowerCase())) || (item.item.displayName.toLowerCase().includes(search_item.toLowerCase()))) && (!item.shopBuysItem) && ((item.stock != 0) || (item.madeOnDemand))) {
+                        else if ((item.item.name.toLowerCase().includes(search_item.toLowerCase()) || item.item.displayName.toLowerCase().includes(search_item.toLowerCase())) && item.shopBuysItem) {
                             results.push({
                                 shop: shop.info,
                                 item: item
@@ -130,135 +173,96 @@ sc.on("command", async (cmd) => {
                         }
                     }
                 }
-                else {
-                    console.warn(`A shop (${shop.info.name}) is broadcasting an empty items array!!`)
-                }
-            }
 
-            if (results.length == 0) {
-                await sc.tell(cmd.user.name, `**Error!** FindShop was unable to find any shops with \`${ search_item }\` in stock. [Why are shops and items missing?](${help_link})`);
-            }
-            else {
-                let printResults = "";
-
-                if (results.length > 5) {
-                    await sc.tell(cmd.user.name, "**Note:** Too many results found. Shorting the list to the first 5 results.");
-                    results.length = 5;
-                }
-
-                for (const result of results) {
-                    printResults += `\n\`${ result.item.item.name }\` at **${ result.shop.name }** (\`${ genCoords(result.shop.location) }\`) for ${ fmt_price(result.item) } (\`${ result.item.stock }\` in stock)`;
-                }
-
-                await sc.tell(cmd.user.name, `Here's what we found for \`${ search_item }\`: ${ printResults }`);
-            }
-        }
-        else if ((cmd.args[0] == "sell") || (cmd.args[0] == "sl")) {
-            // Find shops buying search_item
-            const search_item = cmd.args[1];
-            console.log(`Searching for shops buying ${ search_item }`);
-
-            const shops = await fetchData();
-            const results = [];
-            for (const shop of shops) {
-                for (const item of shop.items) {
-                    if (item.item.name == null) {
-                        console.warn(`A shop (${shop.info.name}) is missing an item name!!`);
-                    }
-                    else if ((item.item.name.toLowerCase().includes(search_item.toLowerCase()) || item.item.displayName.toLowerCase().includes(search_item.toLowerCase())) && item.shopBuysItem) {
-                        results.push({
-                            shop: shop.info,
-                            item: item
-                        })
-                    }
-                }
-            }
-
-            if (results.length == 0) {
-                await sc.tell(cmd.user.name, `**Error!** FindShop was unable to find any shops buying \`${ search_item }\`. [Why are shops and items missing?](${help_link})`);
-            }
-            else {
-                let printResults = "";
-
-                if (results.length > 5) {
-                    await sc.tell(cmd.user.name, "**Note:** Too many results found. Shorting the list to the first 5 results.");
-                    results.length = 5;
-                }
-
-                for (const result of results) {
-                    printResults += `\n\`${ result.item.item.name }\` at **${ result.shop.name }** (\`${ genCoords(result.shop.location) }\`) for ${ fmt_price(result.item) }`;
-                }
-
-                await sc.tell(cmd.user.name, `Here's what we found for \`${ search_item }\`: ${ printResults }`);
-            }
-        }
-        else if ((cmd.args[0] == "shop") || (cmd.args[0] == "sh")) {
-            // Find shop named search_name
-            const search_name = cmd.args[1];
-            console.log(`Searching for shops named ${ search_name }`);
-
-            const shops = await fetchData();
-            const results = [];
-            for (const shop of shops) {
-                if (shop.info.name.toLowerCase().includes(search_name.toLowerCase())) {
-                    results.push(shop)
-                }
-            }
-
-            if (results.length == 0) {
-                await sc.tell(cmd.user.name, `**Error!** FindShop was unable to find any shops named \`${ search_name }\`. [Why are shops and items missing?](${help_link})`);
-            }
-            else {
-                let printResults = "";
-                if (((results.length > 1) && (cmd.args[2] == null)) || ((cmd.args[2] != null) && (Number(cmd.args[2]) > results.length))) {
-                    for (let i = 0; i < results.length; i++) {
-                        printResults += `\n(\`${ i + 1 }\`) ${ results[i].info.name }`
-                    }
-
-                    await sc.tell(cmd.user.name, `Multiple shops were found. Run \`\\fs sh ${ search_name } [number]\` to see specific information. ${ printResults }`);
+                if (results.length == 0) {
+                    await sc.tell(cmd.user.name, `**Error!** FindShop was unable to find any shops buying \`${ search_item }\`. [Why are shops and items missing?](${help_link})`);
                 }
                 else {
-                    let display_shop_idx = 1;
-                    if (cmd.args[2] != null) {
-                        display_shop_idx = Number(cmd.args[2]);
-                    }
-                    const display_shop = results[display_shop_idx - 1];
+                    let printResults = "";
 
-                    printResults = `**${ display_shop.info.name }**`;
-                    if (display_shop.info.owner) {
-                        printResults += ` *by ${ display_shop.info.owner }*`;
-                    }
-                    printResults += `\n`;
-
-                    if (display_shop.info.location) {
-                        printResults += `Located at \`${ genCoords(display_shop.info.location) }\``
-                        if (display_shop.info.location.dimension) {
-                            printResults += ` in the \`${ display_shop.info.location.dimension }\``
-                        }
-                        if (display_shop.info.otherLocations) {
-                            printResults += `+ \`${ display_shop.info.otherLocations.length }\` other locations`
-                        }
-                        printResults += `\n`;
+                    if (results.length > 5) {
+                        await sc.tell(cmd.user.name, "**Note:** Too many results found. Shorting the list to the first 5 results.");
+                        results.length = 5;
                     }
 
-                    if (display_shop.info.software) {
-                        printResults += `Running \`${ display_shop.info.software.name }\``;
-                        if (display_shop.info.software.version) {
-                            printResults += ` v\`${ display_shop.info.software.version }\``;
-                        }
-                        printResults += `\n`;
+                    for (const result of results) {
+                        printResults += `\n\`${ result.item.item.name }\` at **${ result.shop.name }** (\`${ genCoords(result.shop.location) }\`) for ${ fmt_price(result.item) }`;
                     }
 
-                    printResults += `Selling \`${ display_shop.items.length }\` items`;
-                    await sc.tell(cmd.user.name, printResults);
+                    await sc.tell(cmd.user.name, `Here's what we found for \`${ search_item }\`: ${ printResults }`);
                 }
             }
+            else if ((cmd.args[0] == "shop") || (cmd.args[0] == "sh")) {
+                // Find shop named search_name
+                const search_name = cmd.args[1];
+
+                const shops = await fetchData();
+                const results = [];
+                for (const shop of shops) {
+                    if (shop.info.name.toLowerCase().includes(search_name.toLowerCase())) {
+                        results.push(shop)
+                    }
+                }
+
+                if (results.length == 0) {
+                    await sc.tell(cmd.user.name, `**Error!** FindShop was unable to find any shops named \`${ search_name }\`. [Why are shops and items missing?](${help_link})`);
+                }
+                else {
+                    let printResults = "";
+                    if (((results.length > 1) && (cmd.args[2] == null)) || ((cmd.args[2] != null) && (Number(cmd.args[2]) > results.length))) {
+                        for (let i = 0; i < results.length; i++) {
+                            printResults += `\n(\`${ i + 1 }\`) ${ results[i].info.name }`
+                        }
+
+                        await sc.tell(cmd.user.name, `Multiple shops were found. Run \`\\fs sh ${ search_name } [number]\` to see specific information. ${ printResults }`);
+                    }
+                    else {
+                        let display_shop_idx = 1;
+                        if (cmd.args[2] != null) {
+                            display_shop_idx = Number(cmd.args[2]);
+                        }
+                        const display_shop = results[display_shop_idx - 1];
+
+                        printResults = `**${ display_shop.info.name }**`;
+                        if (display_shop.info.owner) {
+                            printResults += ` *by ${ display_shop.info.owner }*`;
+                        }
+                        printResults += `\n`;
+
+                        if (display_shop.info.location) {
+                            printResults += `Located at \`${ genCoords(display_shop.info.location) }\``
+                            if (display_shop.info.location.dimension) {
+                                printResults += ` in the \`${ display_shop.info.location.dimension }\``
+                            }
+                            if ((display_shop.info.otherLocations) && (display_shop.info.otherLocations.length > 0)) {
+                                printResults += ` +\`${ display_shop.info.otherLocations.length }\` other locations`
+                            }
+                            printResults += `\n`;
+                        }
+
+                        if (display_shop.info.software) {
+                            printResults += `Running \`${ display_shop.info.software.name }\``;
+                            if (display_shop.info.software.version) {
+                                printResults += ` v\`${ display_shop.info.software.version }\``;
+                            }
+                            printResults += `\n`;
+                        }
+
+                        printResults += `Selling \`${ display_shop.items.length }\` items`;
+                        await sc.tell(cmd.user.name, printResults);
+                    }
+                }
+            }
+        }
+        catch(err) {
+            console.error(err);
+            await sc.tell(cmd.user.name, `An error occurred!\n\`\`\`${ err }\`\`\``);
         }
     }
 });
 
 sc.on("ready", () => {
-    console.log("Started FindShop Chatbox Server");
+    console.log("Started FindShop Chatbox Server!");
 });
 
 sc.connect();
