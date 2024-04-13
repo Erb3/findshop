90c2a125721ac3a66569f6d7ebc95c5b529153af
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-04-13 18:53:54 +0200

    refactor: patriiks rewrite
    Co-authored-by: PatriikPlays <61064909+PatriikPlays@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -2,28 +2,28 @@ import { z } from "zod";
 import { FindShopLogger } from "./logger";
 
 export const configSchema = z.object({
-  CHATBOX_TOKEN: z.string().uuid(),
-  CHATBOX_NAME: z.string().default("&6&lFindShop"),
-  ALIASES: z.preprocess(
-    (v) => (typeof v === "string" ? v.split(",") : v),
-    z.array(z.string()).default(["fs", "findshop", "fs2"])
-  ),
-  RESULTS_PER_AGE: z.number().default(7),
-  GITHUB_LINK: z.string().default("https://github.com/Pixium/findshop"),
-  CHAT_WIDTH: z.number().default(49),
-  WEBSOCKET_TOKEN: z.string(),
-  DATABASE_URL: z.string(),
+    CHATBOX_TOKEN: z.string().uuid(),
+    CHATBOX_NAME: z.string().default("&6&lFindShop"),
+    ALIASES: z.preprocess(
+        (v) => (typeof v === "string" ? v.split(",") : v),
+        z.array(z.string()).default(["fs", "findshop", "fs2"])
+    ),
+    RESULTS_PER_AGE: z.number().default(7),
+    GITHUB_LINK: z.string().default("https://github.com/Pixium/findshop"),
+    CHAT_WIDTH: z.number().default(49),
+    WEBSOCKET_TOKEN: z.string(),
+    DATABASE_URL: z.string(),
 });
 
 export async function parseConfig() {
-  const config = await configSchema.safeParseAsync(Bun.env);
+    const config = await configSchema.safeParseAsync(Bun.env);
 
-  if (!config.success) {
-    FindShopLogger.logger.error(
-      `Failed to read environment variables. Provided error: ${config.error}`
-    );
-    process.exit(1);
-  } else {
-    return config.data;
-  }
+    if (!config.success) {
+        FindShopLogger.logger.error(
+            `Failed to read environment variables. Provided error: ${config.error}`
+        );
+        process.exit(1);
+    } else {
+        return config.data;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,142 +1,45 @@
-import { connectToDatabase } from "./db";
 import { FindShopLogger } from "./logger";
+import { connectToDatabase } from "./db";
 import { parseConfig } from "./config";
 import { initChatbox } from "./chatboxHandler";
-import { z } from "zod";
+import { websocketMessageSchema } from "./schemas";
 
 FindShopLogger.logger.info("Starting FindShop backend...");
 const config = await parseConfig();
 const db = await connectToDatabase();
 await initChatbox(config, db);
 
-const validDimensions = ["overworld", "nether", "end"];
-
-export const websocketMessageSchema = z
-  .object({
-    type: z.literal("shop"),
-    shopName: z.string(),
-    shopDescription: z.string().optional(),
-    owner: z.string().optional(),
-    computerID: z.number().int(),
-    multiShop: z.number().int().optional(),
-    softwareName: z.string().optional(),
-    softwareVersion: z.string().optional(),
-    mainLocation: z
-      .object({
-        x: z.number().optional(),
-        y: z.number().optional(),
-        z: z.number().optional(),
-        description: z.string().optional(),
-        dimension: z.string().toLowerCase().transform(v => validDimensions.includes(v) && v || undefined).optional(),
-      })
-      .optional(),
-    items: z.array(
-      z.object({
-        itemID: z.string(),
-        kstPrice: z.number().optional(),
-        tstPrice: z.number().optional(),
-        displayName: z.string(),
-        dynamicPrice: z.boolean(),
-        madeOnDemand: z.boolean(),
-        stock: z.number().optional(),
-        requiresInteraction: z.boolean(),
-        isBuyingItem: z.boolean(),
-        noLimit: z.boolean(),
-      })
-    ),
-  })
-  .refine(
-    (data) =>
-      !data.items.every((v) => {
-        return v.kstPrice === undefined && v.tstPrice === undefined;
-      }),
-    "All items must have either a KST or TST price"
-  );
 
 Bun.serve({
-  fetch(req, server) {
-    if (req.headers.get("Authorization") !== config.WEBSOCKET_TOKEN) {
-      return new Response("Unauthorized", { status: 401 });
-    }
+    fetch(req, server) {
+        if (req.headers.get("Authorization") !== config.WEBSOCKET_TOKEN) {
+            return new Response("Unauthorized", { status: 401 });
+        }
 
-    if (server.upgrade(req)) {
-      FindShopLogger.logger.debug("Client connected!");
-      return;
-    }
+        if (server.upgrade(req)) {
+            FindShopLogger.logger.debug("Client connected!");
+            return;
+        }
 
-    return new Response("Upgrade failed :(", { status: 500 });
-  },
-  websocket: {
-    message: async (ws, msg) => {
-      const tryParse = websocketMessageSchema.safeParse(
-        JSON.parse(msg.toString("utf8"))
-      );
-      if (!tryParse.success) {
-        FindShopLogger.logger.error(
-          `Failed to parse websocket message: ${tryParse.error}`
-        );
-        ws.send(
-          JSON.stringify({
-            ok: false,
-            error: `Failed to parse WebSocket message: ${tryParse.error}`,
-          })
-        );
-        return;
-      }
-
-      FindShopLogger.logger.debug("Parsed WebSocket message");
-
-      const data = tryParse.data;
-      const shop = await db.prisma.shop.findFirst({
-        where: {
-          computerID: data.computerID,
-          multiShop: data.multiShop,
-        },
-      });
-
-      if (!shop) {
-        await db.createShop(data);
-        return;
-      }
-
-      await db.prisma.shop.update({
-        where: {
-          id: shop.id,
-        },
-        data: {
-          description: data.shopDescription,
-          multiShop: data.multiShop,
-          name: data.shopName,
-          owner: data.owner,
-          softwareName: data.softwareName,
-          softwareVersion: data.softwareVersion,
-          mainLocation: {
-            upsert: {
-              create: {
-                description: data.mainLocation?.description,
-                dimension: data.mainLocation?.dimension,
-                x: data.mainLocation?.x,
-                y: data.mainLocation?.y,
-                z: data.mainLocation?.z,
-              },
-              update: {
-                description: data.mainLocation?.description,
-                dimension: data.mainLocation?.dimension,
-                x: data.mainLocation?.x,
-                y: data.mainLocation?.y,
-                z: data.mainLocation?.z,
-              },
-            },
-          },
-          items: {
-            deleteMany: {},
-            createMany: {
-              data: data.items,
-            },
-          },
-        },
-      });
+        return new Response("Upgrade failed :(", { status: 500 });
     },
-  },
-  port: 8080,
-});
+    websocket: {
+        message: async (ws, msg) => {
+            if (msg.length > 1<<20) {
+                FindShopLogger.logger.error("Received too long message");
+                return;
+            }
+
+            const tryParse = websocketMessageSchema.safeParse(JSON.parse(msg.toString("utf8")));
+            if (!tryParse.success) {
+                FindShopLogger.logger.error(`Failed to parse websocket message: ${tryParse.error}`);
+                FindShopLogger.logger.error(msg)
+                return;
+            }
+            FindShopLogger.logger.debug("Parsed WebSocket message");
+
+            db.handlePacket(tryParse.data)
+        },
+    },
+    port: 8080
+})
